25bd542dc8b8f3fda17bc6039289ed2cd74ec479
yungsuKo <yungsu2391@gmail.com>
2024-08-15 18:21:48 +0900

    header 수정

--- components/Header.js
+++ components/Header.js
@@ -7,59 +7,18 @@ import {
   MenubarShortcut,
   MenubarTrigger,
 } from '@/components/ui/menubar';
+import Image from 'next/image';
 
 export default function Header() {
   return (
-    <div className="top-0 sticky mt-4">
-      <div className="flex border-gray-100 border-2 p-2 rounded-xl  max-w-3xl  mx-auto">
-        <Menubar className="flex justify-between px-4 gap-4 border-none">
-          <MenubarMenu>
-            <MenubarTrigger className="hover:cursor-pointer bg-gray-800 rounded-l">
-              File
-            </MenubarTrigger>
-            <MenubarContent className="bg-black">
-              <MenubarItem>
-                New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-              </MenubarItem>
-              <MenubarItem>New Window</MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem>Share</MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem>Print</MenubarItem>
-            </MenubarContent>
-          </MenubarMenu>
-          <MenubarMenu>
-            <MenubarTrigger className="hover:cursor-pointer bg-gray-800 rounded-l">
-              File
-            </MenubarTrigger>
-            <MenubarContent className="bg-black">
-              <MenubarItem>
-                New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-              </MenubarItem>
-              <MenubarItem>New Window</MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem>Share</MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem>Print</MenubarItem>
-            </MenubarContent>
-          </MenubarMenu>
-          <MenubarMenu>
-            <MenubarTrigger className="hover:cursor-pointer bg-gray-800 rounded-l">
-              File
-            </MenubarTrigger>
-            <MenubarContent className="bg-black">
-              <MenubarItem>
-                New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-              </MenubarItem>
-              <MenubarItem>New Window</MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem>Share</MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem>Print</MenubarItem>
-            </MenubarContent>
-          </MenubarMenu>
-        </Menubar>
-        <div className="flex-1 bg-white"></div>
+    <div className="top-0 sticky border-b-2 border-gray-400">
+      <div className="flex px-12 py-4  mx-auto ">
+        <div className="flex items-center">로고</div>
+        <div className="flex-1 flex justify-end gap-8">
+          <div>aaa</div>
+          <div>bbb</div>
+          <div>ccc</div>
+        </div>
       </div>
     </div>
   );
