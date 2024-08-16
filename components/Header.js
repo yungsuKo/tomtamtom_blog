d6aad0d1fed417c946225eac614ad6a8db4cb2ef
yungsuKo <yungsu2391@gmail.com>
2024-08-16 19:03:47 +0900

    header mobile css

--- components/Header.js
+++ components/Header.js
@@ -8,12 +8,15 @@ import {
   MenubarTrigger,
 } from '@/components/ui/menubar';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div className="top-0 sticky border-b-2 border-gray-400">
+    <div className="top-0 sticky border-b-2 border-gray-400 bg-black">
       <div className="flex px-12 py-4  mx-auto ">
-        <div className="flex items-center">로고</div>
+        <Link href={'/'}>
+          <div className="flex items-center">로고</div>
+        </Link>
         <div className="flex-1 flex justify-end gap-8">
           <div>aaa</div>
           <div>bbb</div>
--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <div>
       <Header />
-      <div className="mx-auto max-w-2xl py-8">{children}</div>
+      <div className="mx-auto max-w-2xl py-8 px-4 md:px-0">{children}</div>
     </div>
   );
 }
