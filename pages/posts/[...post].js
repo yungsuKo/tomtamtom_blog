30b71e074e44c4cf93a42beaace5b2fe91093f85
yungsuKo <yungsu2391@gmail.com>
2024-08-19 20:01:06 +0900

    [...post] 파일 불필요한 코드 정리

--- pages/posts/[...post].js
+++ pages/posts/[...post].js
@@ -1,13 +1,7 @@
 import { Post, allPosts } from '.contentlayer/generated';
 import { useMDXComponent } from 'next-contentlayer/hooks';
-import Link from 'next/link';
-
-const mdxComponents = {
-  a: ({ href, children }) => <Link href={href}>{children}</Link>,
-};
 
 export default function PostDetail({ params }) {
-  console.log(params);
   const path = params.post.join('/');
   const post = allPosts.find((post) => {
     return post._raw.flattenedPath === path;
@@ -15,15 +9,17 @@ export default function PostDetail({ params }) {
   const MDXContent = useMDXComponent(post.body.code);
 
   return (
-    <article className="mx-auto prose">
-      <div className="mb-8 text-center">
-        <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
-          {new Intl.DateTimeFormat('en-US').format(new Date(post.date))}
-        </time>
-        <h1 className="text-3xl font-bold">{post.title}</h1>
-      </div>
-      <MDXContent className="max-w-xl" />
-    </article>
+    <>
+      <article className="mx-auto prose">
+        <div className="mb-8 text-center">
+          <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
+            {new Intl.DateTimeFormat('en-US').format(new Date(post.date))}
+          </time>
+          <h1 className="text-3xl font-bold">{post.title}</h1>
+        </div>
+        <MDXContent className="max-w-xl" />
+      </article>
+    </>
   );
 }
 
